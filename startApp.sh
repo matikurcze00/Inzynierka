66f93986e06c739f2e906a78018a2678af21856e
mateusz.roszkowski <mateusz.roszkowski@asseco.pl>
2023-08-03 20:22:09 +0200

    Usuniecie starego skryptu

--- startApp.sh
+++
@@ -1,8 +0,0 @@
-echo "Starting Spring Boot server..."
-cd API
-./mvnw.cmd spring-boot:run & 
-# Start the Angular project
-echo "Starting Angular project..."
-cd ../GUI/inz-gui/
-npm install
-npm start & 
